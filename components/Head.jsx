5a9d7c5f57079b21dec41812962a7d4793262913
ShreedharJoshi31 <shreedharjoshi03@gmail.com>
2024-07-08 12:45:29 +0530

    enhanced meta tags

--- components/Head.jsx
+++ components/Head.jsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const CustomHead = ({ title }) => {
   return (
@@ -6,19 +6,22 @@ const CustomHead = ({ title }) => {
       <title>{title}</title>
       <meta
         name="description"
-        content="Nitin Ranganath is an avid full stack web developer building websites and applications you'd love to use"
+        content="Shreedhar Joshi is an avid full stack web developer building websites and applications you'd love to use"
       />
       <meta
         name="keywords"
-        content="nitin ranganath, nitin, ranganath, web developer portfolio, nitin web developer, nitin developer, mern stack, nitin ranganath portfolio, vscode-portfolio"
+        content="shreedhar joshi, shreedhar, joshi, web developer portfolio, shreedhar web developer, shreedhar developer, mern stack, shreedhar joshi portfolio"
       />
-      <meta property="og:title" content="Nitin Ranganath's Portfolio" />
+      <meta property="og:title" content="Shreedhar Joshi's Portfolio" />
       <meta
         property="og:description"
         content="A full-stack developer building websites that you'd like to use."
       />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta
+        property="og:image"
+        content="https://imgur.com/your-image-url.png"
+      />
+      <meta property="og:url" content="https://your-portfolio-url.com" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
@@ -27,5 +30,5 @@ const CustomHead = ({ title }) => {
 export default CustomHead;
 
 CustomHead.defaultProps = {
-  title: 'Nitin Ranganath',
+  title: "Shreedhar Joshi",
 };
