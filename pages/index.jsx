648734cd21ae530589ed9a5a10fa3266f60209fe
ShreedharJoshi31 <shreedharjoshi03@gmail.com>
2024-10-04 21:07:24 +0530

    updated resume link

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,7 +22,7 @@ export default function HomePage() {
               onClick={(e) => {
                 e.preventDefault();
                 window.open(
-                  "https://drive.google.com/file/d/1DVLafITKagIcP4pzJF9ZuddLN6rDBZ0h/view?usp=sharing",
+                  "https://drive.google.com/file/d/1AecgLL_9Ih8vS2r6wS5XIyhM96Pzpmny/view?usp=drive_link",
                   "_blank",
                   "noopener,noreferrer"
                 );
